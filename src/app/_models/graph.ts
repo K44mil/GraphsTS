b0622eec99bcf015d8d7c77b545919f6665bb427
K44mil <kd155@o2.pl>
2019-08-31 15:50:14 +0200

    Graph model updated.

--- src/app/_models/graph.ts
+++ src/app/_models/graph.ts
@@ -29,6 +29,145 @@ export class Graph {
         this._isSaved = isSaved;
     }
 
+    // Public Methods
+    getVertexById(id: number): Vertex {
+        let result: Vertex = null;
+        if (this._vertexs.length >= id)
+            result = this._vertexs[id];   
+        return result;
+    }
+
+    getLoopById(id: number): Loop {
+        let result: Loop = null;
+        if (this._loops.length >= id)
+            result = this._loops[id];
+        return result;
+    }
+
+    getLoopByVertexId(id: number): Loop {
+        let result: Loop = null;
+        this._loops.forEach(l => {
+            if (l.v === id)
+                result = l;
+        });
+        return result;
+    }
+
+    getEdgeById(id: number): Edge {
+        let result: Edge = null;
+        if (this._edges.length >= id)
+            result = this._edges[id];
+        return result;
+    }
+
+    getEdgesByVertexId(id: number): Edge[] {
+        let result: Edge[] = [];
+        this._edges.forEach(e => {
+            if (e.v1 === id || e.v2 === id)
+                result.push(e);
+        });
+        return result;
+    }
+
+    getEdgesByStartVertexId(id: number): Edge[] {
+        let result: Edge[] = [];
+        this._edges.forEach(e => {
+            if (e.v1 === id)
+                result.push(e);
+        });
+        return result;
+    }
+
+    getEdgesByEndVertexId(id: number): Edge[] {
+        let result: Edge[] = [];
+        this._edges.forEach(e => {
+            if (e.v2 === id)
+                result.push(e);
+        });
+        return result;
+    }
+
+    getEdgeByVertexsIds(v1: number, v2: number): Edge {
+        let result: Edge = null;
+        this._edges.forEach(e => {
+            if (e.v1 === v1 && e.v2 === v2)
+                result = e;
+        });
+        return result;
+    }
+
+    deleteVertexById(id: number): Vertex {
+        let result: Vertex = null;
+        const filterResult = this._vertexs.filter(v => v.id === id)
+        if (filterResult.length > 0)
+            result = filterResult[0];
+        this._vertexs = this._vertexs.filter(v => v.id !== id);
+        this.updateVertexsIds();
+        this.deleteEdgesConnectedToVertex(result.id);
+        return result;
+    }
+
+    deleteEdgeById(id: number): Edge {
+        let result: Edge = null;
+        const filterResult = this._edges.filter(e => e.id === id);
+        if (filterResult.length > 0)
+            result = filterResult[0];
+        this._edges = this._edges.filter(e => e.id !== id);
+        this.updateEdgesIds();
+        return result;
+    }
+
+    deleteLoopById(id: number): Loop {
+        let result: Loop = null;
+        const filterResult = this._loops.filter(l => l.id === id);
+        if (filterResult.length > 0)
+            result = filterResult[0];
+        this._loops = this._loops.filter(l => l.id !== id);
+        this.updateLoopsIds();
+        return result;
+    }
+
+    // Private Methods
+    private deleteEdgesConnectedToVertex(id: number): Edge[] {
+        let result: Edge[] = this.getEdgesByVertexId(id);
+        result.forEach(e => {
+            this.deleteEdgeById(e.id);
+        });
+        return result;
+    }
+
+    private updateVertexsIds(): void {
+        let id = 0;
+        this._vertexs.forEach(v => {
+          this.updateEdgesVertexId(v.id, id);
+          v.id = id++;
+        });
+    }
+
+    private updateEdgesIds(): void {
+        let id = 0;
+        this._edges.forEach(e => {
+            e.id = id++;
+        });
+    }
+
+    private updateLoopsIds(): void {
+        let id = 0;
+        this._loops.forEach(l => {
+            l.id = id++;
+        });
+    }
+
+    private updateEdgesVertexId(oldId: number, newId: number): void {
+        this._edges.forEach(e => {
+            if (e.v1 === oldId) {
+              e.v1 = newId;
+            } else if (e.v2 === oldId) {
+              e.v2 = newId;
+            }
+          });
+    }
+
     // Getters and Setters
     get id(): number {
         return this._id;
